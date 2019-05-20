59e9f700a752353c6357defda12ffda9a0404e66
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2019-05-20 22:31:16 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,19 @@
+# alignText README
+
+VS Code extension "alignText"
+
+## Features
+
+The extension permits to align specific text token within selected lines
+
+
+![alt text](alignText.gif)
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+- Initial version
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,56 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerTextEditorCommand('extension.alignTokens',
+                                                               alignTokens);
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
+
+async function alignTokens(textEditor: vscode.TextEditor,
+                           edit: vscode.TextEditorEdit,
+                           ...args: any[]) {
+    vscode.window.showInputBox({ prompt: 'Provide text to align:',
+                                 value: '=' }).then(token => {
+        if (token !== undefined && token.length > 0) {
+            let selection = textEditor.selection;
+
+            let farthestPosition = findFarthestPosition(selection, token);
+            if (farthestPosition !== -1) {
+                alignAccordingToFarthestPosition(selection, token, farthestPosition);
+            }
+        }
+    });
+
+    function findFarthestPosition(selection: vscode.Selection, token: string) {
+        let farestPosition = -1;
+        for (let lineNo = selection.start.line; lineNo <= selection.end.line; ++lineNo) {
+            let lineText = textEditor.document.lineAt(lineNo).text;
+            let foundAt  = lineText.indexOf(token);
+            if (foundAt > farestPosition) {
+                farestPosition = foundAt;
+            }
+        }
+        return farestPosition;
+    }
+
+    function alignAccordingToFarthestPosition(selection: vscode.Selection,
+                                              token: string,
+                                              farthestPosition: number) {
+        textEditor.edit(editBuilder => {
+            for (let lineNo = selection.start.line; lineNo <= selection.end.line; ++lineNo) {
+                let line     = textEditor.document.lineAt(lineNo);
+                let lineText = line.text;
+                let foundAt  = lineText.indexOf(token);
+                if (foundAt !== -1) {
+                    lineText = lineText.substring(0, foundAt) +
+                               ' '.repeat(farthestPosition - foundAt) +
+                               lineText.substring(foundAt);
+                    editBuilder.replace(line.range, lineText);
+                }
+            }
+        });
+    }
+}
